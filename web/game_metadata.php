d5045c3fb722d821fcd397321ab971da8fc5c923
Andrew Hampe <andrew.hampe@gmail.com>
2010-06-12 15:36:32 -0500

    Functionality about to be merged with another file

--- web/game_metadata.php
+++
@@ -1,6 +0,0 @@
-<?php
-require(dirname($_SERVER['SCRIPT_FILENAME']) . '/game_parser.php');
-
-header('Content-Type: text/xml; charset=utf-8');
-header('Content-Disposition: attachment; filename="game_metadata.xml"');
-echo get_metadata();
