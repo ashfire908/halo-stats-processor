e70536128874a40122c16bb3e107061db7ceaaef
Andrew Hampe <andrew.hampe@gmail.com>
2010-06-13 15:13:07 -0500

    Web files updated to be XHTML vaild and to work with updates to parser. Raw code updated to be able to dump more than just the parser. Web page for dumping metadata merged with dumping game xml, file renamed.

--- web/game_data.php
+++ web/game_data.php
@@ -1,5 +1,5 @@
 <?php
-require(dirname($_SERVER['SCRIPT_FILENAME']) . '/game_parser.php');
+require(dirname($_SERVER['SCRIPT_FILENAME']) . '/../game_parser.php');
 function btt($var) {
   if ($var === true) {
     return 'Yes';
@@ -7,11 +7,13 @@ function btt($var) {
     return 'No';
   }
 }
-?>
-<html>
+// In case short tag is enabled
+echo '<?xml version="1.0" encoding="UTF-8" ?>';?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <title>ODST Game Data - Results for gameid <?php echo $_GET['gameid']; ?></title>
-  <link rel="stylesheet" href="odst_data.css" type="text/css">
+  <link rel="stylesheet" href="odst_data.css" type="text/css" />
 </head>
 <body>
   <div class="header">
@@ -36,29 +38,31 @@ if ($gamenum == '') {
   print "Error: No Game ID given. Aborting...";
   trigger_error("No Game ID given", E_USER_ERROR);
 }
-$soap_data = get_data(array($gamenum));
-$game_parsed = parse_data($soap_data[$gamenum]);?> Done.</p>
+$game = new ODSTGame;
+$game->get_game($gamenum);
+$game->load_game();
+?> Done.</p>
 
   <h3>Error</h3>
   <dl>
     <dt>Error:</dt>
-    <dd><?php echo btt($game_parsed->error);?><br></dd>
+    <dd><?php echo btt($game->error);?><br /></dd>
     
     <dt>Status Code:</dt>
-    <dd><?php echo $game_parsed->error_details[0];?><br></dd>
+    <dd><?php echo $game->error_details[0];?><br /></dd>
     
     <dt>Reason:</dt>
-    <dd><?php echo $game_parsed->error_details[1];?><br></dd>
+    <dd><?php echo $game->error_details[1];?><br /></dd>
   </dl>
 <?php
-if ($game_parsed->error === false) {
+if ($game->error === false) {
 ?>
 
   <h3>General Data</h3>
   <dl>
     <dt>Difficulty:</dt>
     <dd><?php
-switch ($game_parsed->difficulty) {
+switch ($game->difficulty) {
   case 0:
     echo 'Easy';
     break;
@@ -72,61 +76,61 @@ switch ($game_parsed->difficulty) {
     echo 'Legendary';
     break;
 }
-?><br></dd>
+?><br /></dd>
     
     <dt>Duration (in seconds):</dt>
-    <dd><?php echo $game_parsed->duration;?><br></dd>
+    <dd><?php echo $game->duration;?><br /></dd>
     
     <dt>Date/Time:</dt>
-    <dd><?php echo $game_parsed->datetime->format('Y-m-d H:i:s');?><br></dd>
+    <dd><?php echo $game->datetime->format('Y-m-d H:i:s');?><br /></dd>
     
     <dt>Map:</dt>
-    <dd><?php echo $game_parsed->map;?><br></dd>
+    <dd><?php echo $game->map;?><br /></dd>
     
     <dt>Scoring Enabled?</dt>
-    <dd><?php echo btt($game_parsed->scoring_enabled);?><br></dd>
+    <dd><?php echo btt($game->scoring_enabled);?><br /></dd>
     
-<?php if ($game_parsed->scoring_enabled) { ?>
+<?php if ($game->scoring_enabled) { ?>
     <dt>Score:</dt>
-    <dd><?php echo $game_parsed->score;?><br></dd>
+    <dd><?php echo $game->score;?><br /></dd>
     
     <dt>Time Bonus:</dt>
-    <dd><?php echo $game_parsed->time_bonus;?><br></dd>
+    <dd><?php echo $game->time_bonus;?><br /></dd>
 <?php } ?>
     
     <dt>Firefight Game?</dt>
-    <dd><?php echo btt($game_parsed->firefight);?><br></dd>
+    <dd><?php echo btt($game->firefight);?><br /></dd>
   </dl>
   
   <div class="header">
     <h3>Players</h3>
-    <p>Number of players: <?php echo $game_parsed->player_count;?></p>
+    <p>Number of players: <?php echo $game->player_count;?></p>
   </div>
 <?php
-foreach ($game_parsed->players as $player) {
+foreach ($game->players as $player) {
 ?>    <div class="subdata">
       <h4>Player ID <?php echo $player->id;?></h4>
       <dl>
         <dt>Gamertag</dt>
-        <dd><?php echo $player->gamertag;?><br></dd>
+        <dd><?php echo $player->gamertag;?><br /></dd>
   
         <dt>Service Tag</dt>
-        <dd><?php echo $player->service_tag;?><br></dd>
+        <dd><?php echo $player->service_tag;?><br /></dd>
   
         <dt>Score</dt>
-        <dd><?php echo $player->score;?><br></dd>
+        <dd><?php echo $player->score;?><br /></dd>
   
         <dt>Kills</dt>
-        <dd><?php echo $player->kills;?><br></dd>
+        <dd><?php echo $player->kills;?><br /></dd>
     
         <dt>Deaths</dt>
-        <dd><?php echo $player->deaths;?><br></dd>
+        <dd><?php echo $player->deaths;?><br /></dd>
   
         <dt>Suicides</dt>
-        <dd><?php echo $player->suicides;?><br></dd>
+        <dd><?php echo $player->suicides;?><br /></dd>
     
         <dt>Betrayals</dt>
-        <dd><?php echo $player->betrayals;?><br></dd>
+        <dd><?php echo $player->betrayals;?><br /></dd>
   
         <dt>Weapons Used</dt>
         <dd>
@@ -146,7 +150,7 @@ foreach ($player->weapons_used as $weapon) {
 <?php foreach ($player->medals as $medal => $count) {
 ?>
             <dt><?php echo $medal; ?></dt>
-            <dd><?php echo $count; ?><br></dd>
+            <dd><?php echo $count; ?><br /></dd>
 <?php } 
 ?>
           </dl>
@@ -160,7 +164,7 @@ foreach ($player->weapons_used as $weapon) {
     <h4>Primary Skulls:</h4>
       <ul>
 <?php
-foreach ($game_parsed->skulls_primary_start as $skull) {
+foreach ($game->skulls_primary_start as $skull) {
   echo '        <li>' . $skull . '</li>
 ';
 }
@@ -169,66 +173,66 @@ foreach ($game_parsed->skulls_primary_start as $skull) {
     <h4>Secondary Skulls:</h4>
       <ul>
 <?php
-foreach ($game_parsed->skulls_secondary_start as $skull) {
+foreach ($game->skulls_secondary_start as $skull) {
   echo '        <li>' . $skull . '</li>
 ';
 }
 ?>
       </ul>
     </div>
-<?php if ($game_parsed->firefight == true) { ?>
+<?php if ($game->firefight == true) { ?>
 
   <h3>Waves</h3>
     <dl>
       <dt>Total Waves:</dt>
-      <dd><?php echo $game_parsed->total_waves;?><br></dd>
+      <dd><?php echo $game->total_waves;?><br /></dd>
       
       <dt>Bonus Rounds:</dt>
-      <dd><?php echo $game_parsed->bonus_rounds;?><br></dd>
+      <dd><?php echo $game->bonus_rounds;?><br /></dd>
       
       <dt>Set Reached:</dt>
-      <dd><?php echo $game_parsed->set_reached;?><br></dd>
+      <dd><?php echo $game->set_reached;?><br /></dd>
       
       <dt>Round Reached:</dt>
-      <dd><?php echo $game_parsed->round_reached;?><br></dd>
+      <dd><?php echo $game->round_reached;?><br /></dd>
         
       <dt>Wave Reached:</dt>
-      <dd><?php echo $game_parsed->wave_reached;?><br></dd>
+      <dd><?php echo $game->wave_reached;?><br /></dd>
     </dl>
 
   <h3>Wave Stats</h3>
 <?php
-foreach ($game_parsed->wave_stats as $wave) {
+foreach ($game->wave_stats as $wave) {
 ?>
     <div class="subdata">
       <h4>Wave <?php echo $wave->id;?></h4>
       <dl>
         <dt>Activity?</dt>
-        <dd><?php echo btt($wave->activity);?><br></dd>
+        <dd><?php echo btt($wave->activity);?><br /></dd>
         
         <dt>Wave Length:</dt>
-        <dd><?php echo $wave->length;?><br></dd>
+        <dd><?php echo $wave->length;?><br /></dd>
         
         <dt>Wave Start:</dt>
-        <dd><?php echo $wave->start;?><br></dd>
+        <dd><?php echo $wave->start;?><br /></dd>
           
         <dt>Wave End:</dt>
-        <dd><?php echo $wave->end;?><br></dd>
+        <dd><?php echo $wave->end;?><br /></dd>
           
         <dt>Score:</dt>
-        <dd><?php echo $wave->score;?><br></dd>
+        <dd><?php echo $wave->score;?><br /></dd>
           
         <dt>Kills:</dt>
-        <dd><?php echo $wave->kills;?><br></dd>
+        <dd><?php echo $wave->kills;?><br /></dd>
           
         <dt>Deaths:</dt>
-        <dd><?php echo $wave->deaths;?><br></dd>
+        <dd><?php echo $wave->deaths;?><br /></dd>
           
         <dt>Sucides:</dt>
-        <dd><?php echo $wave->suicides;?><br></dd>
+        <dd><?php echo $wave->suicides;?><br /></dd>
           
         <dt>Betrayals:</dt>
-        <dd><?php echo $wave->betrayals;?><br></dd>
+        <dd><?php echo $wave->betrayals;?><br /></dd>
         
         <dt>Weapons Used</dt>
         <dd>
@@ -248,7 +252,7 @@ foreach ($wave->weapons_used as $weapon) {
 <?php foreach ($wave->medals as $medal => $count) {
 ?>
             <dt><?php echo $medal; ?></dt>
-            <dd><?php echo $count; ?><br></dd>
+            <dd><?php echo $count; ?><br /></dd>
 <?php } 
 ?>
           </dl>
@@ -263,7 +267,7 @@ foreach ($wave->weapons_used as $weapon) {
     <h3>Weapons Used:</h3>
       <ul>
 <?php
-foreach ($game_parsed->weapons_used as $weapon) {
+foreach ($game->weapons_used as $weapon) {
   echo '        <li>' . $weapon . '</li>
 ';
 }
@@ -272,10 +276,10 @@ foreach ($game_parsed->weapons_used as $weapon) {
 
     <h3>Medals:</h3>
       <dl>
-<?php foreach ($game_parsed->medals as $medal => $count) {
+<?php foreach ($game->medals as $medal => $count) {
 ?>
         <dt><?php echo $medal; ?></dt>
-        <dd><?php echo $count; ?><br></dd>
+        <dd><?php echo $count; ?><br /></dd>
 <?php } 
 ?>
       </dl>
--- web/raw_code.php
+++ web/raw_code.php
@@ -1,4 +1,15 @@
 <?php
 header('Content-Type: text/plain');
-header('Content-Disposition: attachment; filename="game_parser.php"');
-readfile(dirname($_SERVER['SCRIPT_FILENAME']) . '/game_parser.php');
+switch ($_GET['file']) {
+	case 'parser':
+		$file = '../game_parser.php';
+		break;
+	case 'data':
+	    $file = 'game_data.php';
+	    break;
+	default:
+		$file = '../game_parser.php';
+		break;
+}
+header("Content-Disposition: attachment; filename=\"${file}\"");
+readfile(dirname($_SERVER['SCRIPT_FILENAME']) . "/${file}");
