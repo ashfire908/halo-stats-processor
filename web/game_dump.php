0f95625ba8af2613446805471181ff3b759ac1d8
Andrew Hampe <andrew.hampe@gmail.com>
2010-06-13 15:11:14 -0500

    Part of next commit

--- web/game_dump.php
+++ web/game_dump.php
@@ -1,10 +1,24 @@
 <?php
-require(dirname($_SERVER['SCRIPT_FILENAME']) . '/game_parser.php');
-$gameid = $_GET['gameid'];
-if ($gameid == '') {
-  print "Error: No Game ID given. Aborting...";
-  trigger_error("No Game ID given", E_USER_ERROR);
+require(dirname($_SERVER['SCRIPT_FILENAME']) . '/../game_parser.php');
+switch ($_GET['mode']) {
+    case 'game':
+        $gameid = $_GET['gameid'];
+        if ($gameid == '') {
+            echo "Error: No Game ID given. Aborting...";
+            trigger_error("No Game ID given", E_USER_ERROR);
+        }
+        header('Content-Type: text/xml; charset=utf-8');
+        header("Content-Disposition: attachment; filename=\"game_${gameid}.xml\"");
+        echo get_gamexml($gameid);
+        break;
+    case 'metadata':
+        header('Content-Type: text/xml; charset=utf-8');
+        header('Content-Disposition: attachment; filename="game_metadata.xml"');
+        echo get_metadata();
+        break;
+    default:
+    	echo 'No operation selected.';
+    	break;
 }
-header('Content-Type: text/xml; charset=utf-8');
-header("Content-Disposition: attachment; filename=\"game_${gameid}.xml\"");
-echo dump_data($gameid);
+
+
