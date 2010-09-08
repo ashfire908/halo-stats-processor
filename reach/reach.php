d3f52149da8727ae4bf8be8b025b6eafe281807f
Andrew Hampe <andrew.hampe@gmail.com>
2010-09-07 23:59:35 -0500

    Started work on Halo: Reach support.

---
+++ reach/reach.php
@@ -0,0 +1,8 @@
+<?php
+require_once('../parser.php');
+// Halo: Reach Parser
+// Shared
+
+// Reach API Settings
+define('REACH_API_KEY', NULL);
+define('REACH_API_JSON_ENDPOINT', 'api/reach/reachapijson.svc');
