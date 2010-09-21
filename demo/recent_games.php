7b42fe1850e9d18cfda228242a51e457a05fb42e
Andrew Hampe <andrew.hampe@gmail.com>
2010-09-21 02:04:19 -0500

    Added "Demo" to the page titles in the demo pages.

--- demo/recent_games.php
+++ demo/recent_games.php
@@ -7,7 +7,7 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <title>Halo SP - Recent <?php 
+  <title>Halo SP Demo - Recent <?php 
 switch ($_GET['game']) {
     case GAME_HALO_3:
         echo 'Halo 3';
