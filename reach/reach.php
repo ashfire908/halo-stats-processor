393e9cafa13ddb3bbb125debc687e031e90edef7
Andrew Hampe <andrew.hampe@gmail.com>
2010-09-30 20:07:23 -0500

    Added check error function for Reach.

--- reach/reach.php
+++ reach/reach.php
@@ -19,6 +19,53 @@ class ReachBase {
         return json_encode($this->json_data);
     }
     
+    function check_error() {
+        // Check for JSON and API errors
+        
+        // Check for JSON error
+        switch (json_last_error()) {
+            case JSON_ERROR_NONE:
+                // No error
+                break;
+            case JSON_ERROR_DEPTH:
+                $this->error = True;
+                $this->error_details(-2, 'JSON Error - Maximum stack depth has been exceeded');
+                break;
+            case JSON_ERROR_CTRL_CHAR:
+                $this->error = True;
+                $this->error_details(-2, 'JSON Error - Control character error (possibly incorrectly encoded)');
+                break;
+            case JSON_ERROR_STATE_MISMATCH:
+                $this->error = True;
+                $this->error_details(-2, 'JSON Error - Invalid or malformed JSON');
+                break;
+            case JSON_ERROR_SYNTAX:
+                $this->error = True;
+                $this->error_details(-2, 'JSON Error - Syntax error');
+                break;
+            case JSON_ERROR_UTF8:
+                $this->error = True;
+                $this->error_details(-2, 'JSON Error - Malformed UTF-8 characters (possibly incorrectly encoded)');
+                break;
+        }
+        
+        // Stop if we found an error
+        if ($this->error == True) {
+            return;
+        }
+        
+        // Check for API error
+        $this->error_details[0] = $this->json_data->status;
+        $this->error_details[1] = $this->json_data->reason;
+        
+        if ($this->json_data->status != 0) {
+            // Error
+            $this->error = True;
+        }
+        
+        return;
+    }
+    
     // JSON data
     public $json_data;
     
